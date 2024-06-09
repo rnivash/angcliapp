0363ba13665214405eb718d4d68f5c183008db65
Nivash <nivash@debian.debian>
2024-06-10 02:09:40 +0530

    without module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   template: `<h1>Hello world!</h1>`
 })
 export class appComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appComponent } from './app/app.component'
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(appComponent, {providers: []})
+.catch(err => console.error(err));
