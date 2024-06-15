3c8fce71591641d9509540d95f363006f84f4a3f
Nivash <nivash@debian.debian>
2024-06-15 23:54:55 +0530

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<section class="products">
-            <!-- Product listing goes here -->
-            <article class="product">
-                <img src="product1.jpg" alt="Product 1">
-                <h2>Product 1 Name</h2>
-                <p>Description of Product 1</p>
-                <p>Price: $XX.XX</p>
-                <button>Add to Cart</button>
-            </article>
-            <!-- Repeat the above article structure for each product -->
-        </section>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 
 export class appComponent {
